4640e95df901ce543da2a18f88ccbada47b28bc5
bruno filadelfo <bruno.filadelfo@outlook.com>
2023-07-03 06:12:56 -0300

    Python

---
+++ copiando_listas.py
@@ -0,0 +1,9 @@
+#comidas saudaveis
+Grãoes_integrais = ['Arroz integral','Quinoa','Aveia em flocos','Massa integral']
+#Alimentos = Grãoes_integrais[:]
+Alimentos = Grãoes_integrais
+#Leguminosas = ['Feijão','Lentilhas','Grão-de-bico','Ervilhas']
+Grãoes_integrais.append('Trigo integral')
+Grãoes_integrais.sort()
+print(Grãoes_integrais)
+print(Alimentos)
