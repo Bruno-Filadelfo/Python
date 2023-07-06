32e381e02d6b132c8387e00d3aaa7b416adc68d7
bruno filadelfo <bruno.filadelfo@outlook.com>
2023-07-05 23:16:55 -0300

    Python

---
+++ if_else.py
@@ -0,0 +1,12 @@
+cor = "Amarelo"
+if cor == "Laranja":
+    print(cor)
+else:
+    print("Cor não é Laranja")
+
+
+fruta = "Melancia"
+if fruta == "Abacaxi":
+    print(fruta)
+else:
+    print("Essa Fruta não é abacaxi")
